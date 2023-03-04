21060c7060ea7e63bb87356e55aa8555bbf91ce1
Milk <vitalcik.kovalenko2019@gmail.com>
2023-03-04 19:16:45 +0300

    убрал вывод пароля

--- srcs/Appservices/Dtos/Output/UserDto.cs
+++ srcs/Appservices/Dtos/Output/UserDto.cs
@@ -3,7 +3,6 @@ public class UserDto
 {
     public string Login { get; set; } = null!;
     public string Email { get; set; } = null!;
-    public string Password { get; set; } = null!;
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
 }
